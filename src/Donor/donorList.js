0dd7e9b1d843feec6eb8ca4d29993c02848732e4
Hammad Hassan <hammadhassansheikh@gmail.com>
2017-07-06 14:14:17 +0500

    get data from firebase

--- src/Donor/donorList.js
+++ src/Donor/donorList.js
@@ -9,37 +9,65 @@ import {
 } from 'material-ui/Table';
 // import injectTapEventPlugin from 'react-tap-event-plugin';
 // injectTapEventPlugin();
-import firebase from "firebase";
+//import firebase from "firebase";
+import * as firebase from 'firebase';
 
 class DonorList extends Component {
   constructor() {
     super();
     this.state = {
-    donorList: [{
+    donorList: [
+      {
       name: "Hammad Hassan",
       bloodGroup: "B+"
-    }]
+    }
+    ]
     }
     this.getValues = this.getValues.bind(this)
   }
   componentWillMount() {   
+    
     // var rootRef = firebase.database().ref('/').child('donorsData');
     //     rootRef.on('child_added', snap => {
-    var rootRef = firebase.database().ref().child('donorsData');
-        rootRef.on("value", (snap) => {
-            var list = [
-            //name: this.state.donorList.name,
-            //bloodGroup: this.state.donorList.bloodGroup
-            ];
-            list = this.state.donorList;
-            list.push(snap.val().donorList);
-            alert(list)
-            // alert(list);
-            this.setState({
-                donorList: list
-            })
-            //console.log(this.state.donorList);
-        })
+    // var rootRef = firebase.database().ref().child('donorsData');
+    //     rootRef.on("value", (snap) => {
+    //         var list = [
+    //         //name: this.state.donorList.name,
+    //         //bloodGroup: this.state.donorList.bloodGroup
+    //         ];
+    //         list = this.state.donorList;
+    //         list.push(snap.val().donorList);
+    //         alert(list)
+    //         // alert(list);
+    //         this.setState({
+    //             donorList: list
+    //         })
+    //         //console.log(this.state.donorList);
+    //     })
+    //     // console.log(this.state.donorList);
+     var rootRef = firebase.database().ref().child('donorsData');
+    //     rootRef.on('child_added', function (value) {
+            rootRef.on("child_added", function(snapshot) {
+          var details = [
+            {}
+          ];
+          details = snapshot.val();
+          console.log(snapshot.val());
+            console.log(details);
+            //details = this.state.donorList;
+            //alert(donorList);
+  console.log("Author: " + details.name);
+  console.log("Title: " + details.bloodGroup);
+  //console.log(this.state.donorList);
+  //console.log("Previous Post ID: " + prevChildKey);
+
+        //     var list = [];
+        //     list.push(value.val().donorList);
+        //     this.setState({
+        //         donorList: list
+        //     })
+        //     console.log(this.state.donorList);
+        });
         // console.log(this.state.donorList);
     }
   getValues(e) {
